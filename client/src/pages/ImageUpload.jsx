96a0fbfaef1019328a6c249378e8fc6a51118b40
AviralPatel0526 <aviralpatel2603@gmail.com>
2024-04-06 12:17:40 +0530

    popup for images

--- client/src/pages/ImageUpload.jsx
+++ client/src/pages/ImageUpload.jsx
@@ -4,11 +4,13 @@ import dehaze from '../assets/dehazed.png';
 
 function ImageUpload() {
   const [file, setFile] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState(null);
   const [dehazedImagePath, setDehazedImagePath] = useState(null);
   const [showModal, setShowModal] = useState(false);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
+    setUploadedImage(URL.createObjectURL(event.target.files[0])); // Store the uploaded image
   };
 
   const uploadFile = async () => {
@@ -49,9 +51,13 @@ function ImageUpload() {
     }
   };
 
+  const handleClose = () => {
+    setShowModal(false);
+  }
+
   return (
     <div>
-      <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
+      <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8 font-serif">
         <div className="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
           <div className="text-left">
             <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-800 ">
@@ -74,12 +80,33 @@ function ImageUpload() {
                 </button>
               </div>
               {showModal && (
-                <div className="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-gray-700 bg-opacity-50">
-                  <div className="bg-white p-8 max-w-md mx-auto rounded-lg">
-                    <img src={dehazedImagePath} alt="Dehazed" style={{ maxWidth: '100%', maxHeight: '400px' }} />
-                    <button className="mt-4 px-4 py-2 bg-primary text-white rounded-md" onClick={downloadImage}>Download Dehazed Image</button>
+                // <div className="fixed inset-0 z-50 flex flex-col items-center justify-center overflow-auto bg-gray-700 bg-opacity-50">
+                //   <div className="bg-white p-8 max-w-md mx-auto rounded-lg flex">
+                //     <img src={uploadedImage} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '400px' }} />
+                //     <img src={dehazedImagePath} alt="Dehazed" style={{ maxWidth: '100%', maxHeight: '400px' }} />
+                //   </div>
+                //     <button className="mt-4 px-4 py-2 bg-primary text-white rounded-md" onClick={downloadImage}>Download Dehazed Image</button>
+                // </div>
+                <div className='fixed z-50 inset-0 bg-white w-full h-full '>
+                  <button className="fixed right-1 top-1  px-4 py-2 bg-primary text-white rounded-md" onClick={handleClose}>Close</button>
+                  <div className='flex flex-col w-full h-full justify-center items-center'>
+                  <div className='flex gap-1 '>
+                    <div className='text-center'>
+                    <p className='text-primary text-3xl font-bold'>Before</p>
+                    <img src={uploadedImage} className='w-[500px] h-[400px]'/>
+                    </div>
+                    <div className='text-center'>
+                    <p  className='text-primary text-3xl font-bold'>After</p>
+                    <img src={dehazedImagePath}  className='w-[500px] h-[400px]' />
+                    </div>
                   </div>
-                </div>
+                  <div>
+                  <button className="mt-4 px-4 py-2 bg-primary text-white rounded-md" onClick={downloadImage}>Download Dehazed Image</button>
+                  </div>
+                  </div>
+                  
+                  </div>
+               
               )}
             </div>
           </div>
