ecd669122866c8b71ad51c01a21490692b6a08d4
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-04-06 09:15:14 +0530

    added map.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,11 @@ import { Footer } from './components/Footer'
 import Home from './pages/Home'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Upload from './pages/Upload';
+import ImageUpload from './pages/ImageUpload';
+import RealTimeDehaze from './pages/RealTimeDehaze';
+import Video from './pages/Video';
+import LocationData from './pages/Alert';
+import DynamicGoogleMap from './components/Map';
 
 function App() {
 
@@ -13,6 +18,11 @@ function App() {
     <Routes>
     <Route path="/" element={<Home/>} />
     <Route path="/upload" element={<Upload/>} />
+    <Route path="/image-upload" element={<ImageUpload/>} />
+    <Route path='/real-time' element={<RealTimeDehaze/>}/>
+    <Route path='/video-upload' element={<Video/>}/>
+    <Route path='/alert' element={<LocationData/>}/>
+    <Route path='/map' element={<DynamicGoogleMap/>}/>
   
     </Routes>
     <Footer/>
