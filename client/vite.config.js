92f6bba7fa64ccbd1bcba3318dcce501c6027310
Ayush <168056574+ayush22-26@users.noreply.github.com>
2025-09-05 13:00:26 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- This ensures all asset paths are relative for S3/static hosting
 })
